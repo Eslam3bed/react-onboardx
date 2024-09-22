a3917ea19e98e54d8932395c21f789d2c1a51a5f
eslam3bed <20760072+Eslam3bed@users.noreply.github.com>
2024-09-22 19:15:29 +0300

    update the test configrations and the ci/cd

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,12 @@ module.exports = {
     '\\.(css|scss)$': 'identity-obj-proxy',
   },
   setupFilesAfterEnv: ['<rootDir>/src/__test__/setupTests.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
